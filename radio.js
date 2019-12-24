a20e7c0e2825c37a8367cee004a78ff9a56ab871
Joe <joe.studer.18@gmail.com>
2019-12-23 21:06:40 -0600

    Fixing radio formatting, adding a few radios, created a calendar placeholder, and updated the package.json

--- radio.js
+++ radio.js
@@ -1,10 +1,24 @@
-function showRadio(arg_id) {
-  var display = 'flex';
-  var clr = 'blue';
-  if (this.document.getElementById(arg_id).style.display == 'flex') {
-    display = 'none';
-    clr = 'black';
+var openRadio = 0;
+
+function _setOpen(arg_id) {
+  if (openRadio == 0) {
+    openRadio = arg_id;
   }
-  this.document.getElementById(arg_id).style.display = display;
-  this.document.getElementById(arg_id+'btn').style.color = clr;
+}
+
+function showRadio(arg_id) {
+  if (openRadio == arg_id) {
+    // Closes Radio if same button is pressed
+    openRadio = 0;
+    this.document.getElementById(arg_id).style.display = 'none';
+    this.document.getElementById(arg_id+'btn').style.color = 'black';
+  } else {
+    if (openRadio != 0) {
+      this.document.getElementById(openRadio).style.display = 'none';
+      this.document.getElementById(openRadio+'btn').style.color = 'black';    
+      openRadio = 0;
+    }
+    _setOpen(arg_id);
+    this.document.getElementById(arg_id).style.display = 'flex';
+    this.document.getElementById(arg_id+'btn').style.color = 'blue';  }
 }
